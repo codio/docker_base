1498e6b632614c55afea13337c0c2fb940020d99
Maxim Kraev <maxim.kraev@gmail.com>
2013-12-12 11:54:51 +0400

    nginx + mongo

--- home/build.sh
+++ home/build.sh
@@ -9,7 +9,11 @@ sudo docker build -t base ../base/
 $RUN touch /home/codio/.profile
 cp install.sh ./image/
 $RUN ./install.sh
+$RUN mkdir -p /home/codio/var/log/nginx /home/codio/var/lib/mongodb /home/codio/var/log/mongodb /home/codio/etc/nginx/sites-enabled
+cp -r etc ./image
+
 rm ./image/install.sh
 
 mkdir ./image/workspace
+
 #docker rmi base_tmp
