61daa275ef7dec5895750edeca4a0ebaf693a3b2
Maxim Kraev <maxim.kraev@gmail.com>
2013-12-27 09:48:07 +0400

    build.sh fix

--- home/build.sh
+++ home/build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -e
+BASE_HOME="/mnt/data/base-home"
+RUN="docker run -rm -u codio -w /home/codio -v ${BASE_HOME}:/home/codio:rw -i -t base"
 
-RUN="docker run -rm -u codio -w /home/codio -v `pwd`/image:/home/codio:rw -i -t base"
-
-rm -rf ./image
-mkdir -p ./image
-chown 1000 ./image
+rm -rf ${BASE_HOME}
+mkdir -p ${BASE_HOME}
+chown 1002 ${BASE_HOME}
 
 docker build -t base ../base/
 $RUN touch /home/codio/.profile
 chmod +x install.sh
-cp install.sh ./image/
+cp install.sh ${BASE_HOME}/
 $RUN ./install.sh
 $RUN mkdir -p /home/codio/var/log/nginx /home/codio/var/lib/mongodb /home/codio/var/log/mongodb /home/codio/etc/nginx/sites-enabled
-cp -r etc ./image
+cp -r etc ${BASE_HOME}/
 
-rm ./image/install.sh
+rm ${BASE_HOME}/install.sh
 
-$RUN mkdir /home/codio/image/workspace
+$RUN mkdir /home/codio/workspace
 
 #docker rmi base_tmp
