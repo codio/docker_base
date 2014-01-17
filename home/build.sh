2b6a01d880da445e574f484aa834efdc8b3f6e6e
mloginov <mikhail.loginov@citronium.com>
2014-01-17 11:30:16 +0400

    fixed build.sh

--- home/build.sh
+++ home/build.sh
@@ -3,8 +3,8 @@ set -e
 BASE_HOME="/mnt/data/base-home"
 RUN="docker run -rm -u codio -w /home/codio -v ${BASE_HOME}:/home/codio:rw -i -t base"
 
-find ${BASE_HOME} -mindepth 1 -delete
 mkdir -p ${BASE_HOME}
+find ${BASE_HOME} -mindepth 1 -delete
 chown 1100 ${BASE_HOME}
 
 docker build -t base ../base/
