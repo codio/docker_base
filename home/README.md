2861be450e0e8cb34525bd02c532902272bf4ed4
Maxim Kraev <maxim.kraev@gmail.com>
2014-02-12 13:52:35 +0400

    squoshfs was added

--- home/README.md
+++ home/README.md
@@ -5,5 +5,14 @@ run
 ``` bash
 ./build.sh
 ````
+after build base-home.sqsh file will be available.
 
-base image will be in image folder
+to deploy it use:
+s3cmd put --acl-public --guess-mime-type base-home.sqsh s3://codio-base-home-images/base-home-v{version_num}.sqsh
+
+
+mount command
+make sure you have squashfs-tools installed (sudo apt-get install squashfs-tools)
+
+sudo mount base-home-v1.sqsh /mnt/data/base-home -t squashfs -o loop
+
--- home/build.sh
+++ home/build.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -e
-BASE_HOME="/mnt/data/base-home"
-RUN="docker run -rm=true -u codio -w /home/codio -v ${BASE_HOME}:/home/codio:rw codio/base"
+BASE_HOME="/tmp/base-home-tmp"
+RUN="docker run -rm=true -w /home/codio -v ${BASE_HOME}:/home/codio:rw codio/base "
 
+if [ -d "${BASE_HOME}" ]; then
+    rm -rf ${BASE_HOME}
+fi
 mkdir -p ${BASE_HOME}
-find ${BASE_HOME} -mindepth 1 -delete
 chown 1100 ${BASE_HOME}
 
-$RUN touch /home/codio/.profile
+touch ${BASE_HOME}/.profile && chown 1100 ${BASE_HOME}/.profile
 sudo cp .bash_profile ${BASE_HOME}
 sudo chown 1100 ${BASE_HOME}/.bash_profile
 
 chmod +x install.sh
 cp install.sh ${BASE_HOME}/
 
-$RUN ./install.sh
+$RUN su -l codio -c "/bin/bash ./install.sh"
 
 rm ${BASE_HOME}/install.sh
 
-$RUN mkdir /home/codio/workspace
+mkdir ${BASE_HOME}/workspace && chown 1100 ${BASE_HOME}/workspace
+mksquashfs ${BASE_HOME} base-home.sqsh
+
