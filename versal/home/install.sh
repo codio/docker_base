31638fc232ba68d7fdb1c5d8ff80820ffe5c8466
Maxim Kraev <maxim.kraev@gmail.com>
2014-08-11 15:06:56 +0400

    user master for versal

--- versal/home/install.sh
+++ versal/home/install.sh
@@ -8,7 +8,7 @@ eval "$(rbenv init -)"
 export PATH="$HOME/.parts/autoparts/bin:$PATH"
 eval "$(parts init -)"
 
-npm install -g https://github.com/Versal/sdk/tarball/feature/codio-init.zip
+npm install -g git://github.com/Versal/sdk
 npm install -g bower
 parts update
 parts install zip
